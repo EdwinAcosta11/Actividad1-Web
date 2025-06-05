c6d8b1d0776de753045a56c58364c17c24787f58
EdwinAcosta11 <elimperiar@gmail.com>
2025-06-05 18:17:17 -0400

    Agregando Ejercicio 3

---
+++ Actividad1Ejerc34318/Ejercio3/Controllers/TemperatureController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+        // Convierte Celsius a Fahrenheit: /api/temperature/toFahrenheit?celsius=25
+        [HttpGet("toFahrenheit")]
+        public IActionResult ToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return Ok(fahrenheit);
+        }
+
+        // Convierte Fahrenheit a Celsius: /api/temperature/toCelsius?fahrenheit=77
+        [HttpGet("toCelsius")]
+        public IActionResult ToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return Ok(celsius);
+        }
+    }
+}
