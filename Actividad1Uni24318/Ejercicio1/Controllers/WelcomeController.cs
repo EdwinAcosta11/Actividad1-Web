6905712f6e3fc24439de6c8dfbbbeba07b80ad52
EdwinAcosta11 <elimperiar@gmail.com>
2025-06-05 17:38:04 -0400

    Agregando Primer ejercicio WelcomeController

---
+++ Actividad1Uni24318/Ejercicio1/Controllers/WelcomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ejercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        // GET: api/<WelcomeController>
+        [HttpGet]
+        public IActionResult GetwlcomeMessage(String name)
+        {
+            return Ok(string.Format("Hola {0}, un placer conocerte", name));
+        }
+
+       
+    }
+}
