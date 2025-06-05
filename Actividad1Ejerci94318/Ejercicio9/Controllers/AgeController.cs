4d110474ad3b5e9c168f072020266eeeb7372ae0
EdwinAcosta11 <elimperiar@gmail.com>
2025-06-05 18:56:10 -0400

    Agregando ejercicio 9

---
+++ Actividad1Ejerci94318/Ejercicio9/Controllers/AgeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgeController : ControllerBase
+    {
+        // Ruta: /api/age/calculate?birthDate=2000-05-10
+        [HttpGet("calculate")]
+        public IActionResult CalculateAge(DateTime birthDate)
+        {
+            var today = DateTime.Today;
+
+            if (birthDate > today)
+                return BadRequest("La fecha de nacimiento no puede ser en el futuro.");
+
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return Ok(new
+            {
+                BirthDate = birthDate.ToString("yyyy-MM-dd"),
+                Age = age
+            });
+        }
+    }
+}
